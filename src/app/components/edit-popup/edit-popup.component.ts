f17c69140bb9d0e07e581c930a54e8450b807e89
KhaledMahfouz5 <khaledmhfz2004@gmail.com>
2024-03-25 14:11:32 +0000

    Trying To Code like in tutorial . 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/components/edit-popup/edit-popup.component.ts 	modified:   src/app/components/product/product.component.html 	modified:   src/app/components/product/product.component.ts 	modified:   src/app/home/home.component.html We Are At 79% ... in (03:14:08) .. I have failed to get the Right result .. good luck at Real coding 0_0 .

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -15,6 +15,7 @@ import { ButtonModule } from 'primeng/button';
 })
 export class EditPopupComponent {
   @Input() display: boolean = false;
+  @Output() displayChange = new EventEmitter<boolean>()
   @Input() header! : string  ;
   @Output() confirm = new EventEmitter<Product>();
   @Input() product: Product = {
@@ -26,8 +27,11 @@ export class EditPopupComponent {
   };
   onConfirm() {
     this.confirm.emit(this.product);
+    this.display = false ;
+    this.displayChange.emit(this.display);
   }
   onCancel() {
     this.display = false;
+    this.displayChange.emit(this.display);
   }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,23 +1,37 @@
 import { Product } from './../../../types';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {RatingModule} from 'primeng/rating'
 import { ButtonModule } from 'primeng/button';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule],
+  imports: [RatingModule, FormsModule, ButtonModule,ConfirmPopupModule],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private confirmationService:ConfirmationService){}
   @Input() product!: Product;
+  @ViewChild('magicButton') magicButton : any ;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
 
   editProduct() {
     this.edit.emit(this.product);
+  }
+  confirmDelete(){
+    this.confirmationService.confirm({
+      target:this.magicButton.nativeElement ,
+      message:"Are You Sure ??",
+      accept:()=>{
+        this.deleteProduct();
+      },
+    })
   }
   deleteProduct() {
     this.delete.emit(this.product);
